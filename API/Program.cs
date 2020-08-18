f87e349c8a21fa445be65a22f9273d0453805eec
AndreeaYuri <andreea.horlescuu@gmail.com>
2020-08-18 23:19:34 +0100

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -17,7 +12,7 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            var host=
+            var host =
                 CreateHostBuilder(args).Build();
             using (var scope = host.Services.CreateScope())
             {
@@ -26,11 +21,12 @@ namespace API
                 {
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate();
+                    Seed.SeedData(context);
                 }
                 catch (Exception ex)
                 {
-                    var logger = services.GetRequiredService < ILogger<Program>>();
-                    logger.LogError(ex,"An error occured during migration");
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured during migration");
                 }
             }
             host.Run();
