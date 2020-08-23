1d05d8bfcae0dff2fd016dfad8245492a4e92980
AndreeaYuri <andreea.horlescuu@gmail.com>
2020-08-23 22:31:09 +0100

    End of section 5

---
+++ client-app/src/features/nav/NavBar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Menu, Container, Button } from "semantic-ui-react";
+
+interface IProps {
+  openCreateForm: () => void;
+}
+const NavBar: React.FC<IProps> = ({ openCreateForm }) => {
+  return (
+    <Menu fixed="top" inverted>
+      <Container>
+        <Menu.Item>
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "10px" }}
+          />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name="messages" />
+        <Menu.Item>
+          <Button
+            onClick={openCreateForm}
+            positive
+            content="CreativeActivity"
+          />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
+
+export default NavBar;
